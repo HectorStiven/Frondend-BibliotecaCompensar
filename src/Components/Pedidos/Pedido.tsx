e9f91b9d5e2adff5d671c23d59f04835e1323210
HectorStiven <vigoyahecto90@gmail.com>
2024-04-10 19:15:37 -0500

    a

--- src/Components/Pedidos/Pedido.tsx
+++ src/Components/Pedidos/Pedido.tsx
@@ -1,112 +1,128 @@
-  import React, { useState } from 'react';
-  import { Grid, TextField, Button } from '@mui/material';
-  import { DataGrid } from '@mui/x-data-grid';
-  
-  export const PedidoPrincipal = () => {
-    // Estado para almacenar los datos del formulario de búsqueda
-    const [searchData, setSearchData] = useState({
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Grid, TextField, Button } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+
+interface Estudiante {
+  id: number;
+  numero_identidad: string;
+  primer_nombre: string;
+  segundo_nombre: string | null;
+  primer_apellido: string;
+  segundo_apellido: string | null;
+  edad: number;
+  fecha_nacimiento: string;
+  correo_electronico: string;
+  numero_celular: string;
+  pertenece_colegio: boolean;
+  pertenece_universidad: boolean;
+  tipo_documento: string | null;
+  tipo_genero: string | null;
+  colegio: string | null;
+  universidad: string | null;
+  id_grado: number | null;
+}
+
+export const PedidoPrincipal = () => {
+  const [searchData, setSearchData] = useState({
+    nombre: '',
+    libro: '',
+    autor: ''
+  });
+  const [rows, setRows] = useState<Estudiante[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const url = 'https://backend-bibliotecacompensar-production.up.railway.app/estudiantes/obtener_estudiante/';
+      const response = await axios.get(url);
+      setRows(response.data.data);
+      console.log(response.data); // Imprime los datos en la consola
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSearchData({
+      ...searchData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para buscar los datos en la DataGrid
+    console.log('Datos de búsqueda:', searchData);
+    // Limpia los campos después de enviar el formulario de búsqueda
+    setSearchData({
       nombre: '',
       libro: '',
       autor: ''
     });
-  
-    // Datos de ejemplo para la DataGrid
-    const [rows] = useState([
-      { id: 1, nombre: 'Juan', libro: 'La sombra del viento', autor: 'Carlos Ruiz Zafón' },
-      { id: 2, nombre: 'María', libro: 'Cien años de soledad', autor: 'Gabriel García Márquez' },
-      { id: 3, nombre: 'Pedro', libro: 'Harry Potter y la piedra filosofal', autor: 'J.K. Rowling' },
-    ]);
-  
-    // Manejador de cambios en los campos del formulario de búsqueda
-    const handleInputChange = (event:any) => {
-      const { name, value } = event.target;
-      setSearchData({
-        ...searchData,
-        [name]: value
-      });
-    };
-  
-    // Manejador del envío del formulario de búsqueda
-    const handleSubmit = (event:any) => {
-      event.preventDefault();
-      // Aquí puedes agregar la lógica para buscar los datos en la DataGrid
-      console.log('Datos de búsqueda:', searchData);
-      // Limpia los campos después de enviar el formulario de búsqueda
-      setSearchData({
-        nombre: '',
-        libro: '',
-        autor: ''
-      });
-    };
-  
-    // Columnas para la DataGrid
-    const columns = [
-      { field: 'nombre', headerName: 'Nombre', width: 150 },
-      { field: 'libro', headerName: 'Libro', width: 200 },
-      { field: 'autor', headerName: 'Autor', width: 200 },
-    ];
-  
-    return (
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <form onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Nombre"
-                  name="nombre"
-                  value={searchData.nombre}
-                  onChange={handleInputChange}
-                  variant="outlined"
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Libro"
-                  name="libro"
-                  value={searchData.libro}
-                  onChange={handleInputChange}
-                  variant="outlined"
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Autor"
-                  name="autor"
-                  value={searchData.autor}
-                  onChange={handleInputChange}
-                  variant="outlined"
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Autor"
-                  name="autor"
-                  value={searchData.autor}
-                  onChange={handleInputChange}
-                  variant="outlined"
-                  margin="normal"
-                />
-              </Grid>
-            </Grid>
-            <Button type="submit" variant="contained" color="primary">
-              Buscar
-            </Button>
-          </form>
-        </Grid>
-        <Grid item xs={12}>
-          <div style={{ height: 400, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} autoHeight  />
-          </div>
-        </Grid>
-      </Grid>
-    );
   };
-  
+
+  const columns = [
+    { field: 'primer_nombre', headerName: 'Nombre', width: 150 },
+    { field: 'primer_apellido', headerName: 'Apellido', width: 200 },
+    { field: 'correo_electronico', headerName: 'Correo Electrónico', width: 250 },
+    { field: 'numero_celular', headerName: 'Número Celular', width: 200 },
+  ];
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Fetch data on component mount
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Nombre"
+                name="nombre"
+                value={searchData.nombre}
+                onChange={handleInputChange}
+                variant="outlined"
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Libro"
+                name="libro"
+                value={searchData.libro}
+                onChange={handleInputChange}
+                variant="outlined"
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Autor"
+                name="autor"
+                value={searchData.autor}
+                onChange={handleInputChange}
+                variant="outlined"
+                margin="normal"
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" variant="contained" color="primary">
+            Buscar
+          </Button>
+        </form>
+      </Grid>
+      <Grid item xs={12}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid rows={rows} columns={columns} autoHeight />
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
--- src/Elements/App-Bar/ImagenPortada.tsx
+++ src/Elements/App-Bar/ImagenPortada.tsx
@@ -9,7 +9,6 @@ export  const ImagenPortada = () => {
 
 
 
-
 export  const ImagenFondo = () => {
   return (
     <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0 }}>
